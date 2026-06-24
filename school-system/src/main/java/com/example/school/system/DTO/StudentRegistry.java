d478957f1cf9259b1c3768adc1baf82befb8d86c
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-24 18:33:04 +0300

    added student DTO

---
+++ school-system/src/main/java/com/example/school/system/DTO/StudentRegistry.java
@@ -0,0 +1,7 @@
+package com.example.school.system.DTO;
+
+import java.util.Date;
+
+public record StudentRegistry(Long adm, String fullName, Date Regidtration, String status, int PhoneNumber) {
+
+}
