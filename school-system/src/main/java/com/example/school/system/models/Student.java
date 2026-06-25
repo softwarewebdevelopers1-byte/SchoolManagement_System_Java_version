5d646fb96d60c16d3a554a329131919683a18dcd
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-25 17:25:47 +0300

    replaced boiler plate getters and setters with @Getter and @Setter annotations from lombok package

--- school-system/src/main/java/com/example/school/system/models/Student.java
+++ school-system/src/main/java/com/example/school/system/models/Student.java
@@ -10,9 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "student")
+@Getter
+@Setter
 // student entity
 public class Student {
     @Id
@@ -38,71 +42,6 @@ public class Student {
 
     @Column(name = "password")
     String password;
-    // getters and setters
-
-    // for student student id
-
-    public void setStudentId(Long id) {
-        this.studentId = id;
-    }
-
-    public Long getStudentId() {
-        return this.studentId;
-    }
-
-    // for student admission
-    public void setStudentAdm(Long adm) {
-        this.studentAdm = adm;
-    }
-
-    public Long getStudentAdm() {
-        return this.studentAdm;
-    }
-
-    // for student full name
-    public void setFullName(String name) {
-        this.fullName = name;
-    }
-
-    public String getFullName() {
-        return this.fullName;
-    }
-
-    // for student registration date
-    public void setDate(LocalDate registrationDate) {
-        this.date = registrationDate;
-    }
-
-    public LocalDate getDate() {
-        return this.date;
-    }
-
-    // for student account status
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    // for phone number
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    // for hashed passowrd
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
 
     public Student() {
     }
