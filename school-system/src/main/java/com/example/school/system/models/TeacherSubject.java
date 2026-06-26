b823069a60333050935f8946ee872fd23de13165
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-26 23:37:38 +0300

    joined columns

--- school-system/src/main/java/com/example/school/system/models/TeacherSubject.java
+++ school-system/src/main/java/com/example/school/system/models/TeacherSubject.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,9 +25,11 @@ public class TeacherSubject {
 
     // relationship btwn teachersubject and teacher entities
     @ManyToOne
+    @JoinColumn(name = "teacher_id")
     Teacher teacher;
 
     // relationship btwn teachersubject and subject entities
     @ManyToOne
+    @JoinColumn(name = "subject_id")
     Subject subject;
 }
