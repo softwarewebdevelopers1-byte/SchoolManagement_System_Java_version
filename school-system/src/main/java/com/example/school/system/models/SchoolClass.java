4ddde8fbb38f9d2ca29e74dd2c3133534b099219
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-26 16:53:46 +0300

    created a relatiuonship between class and student

--- school-system/src/main/java/com/example/school/system/models/SchoolClass.java
+++ school-system/src/main/java/com/example/school/system/models/SchoolClass.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -39,4 +40,7 @@ public class SchoolClass {
     @ManyToOne
     @JoinColumn(name = "school")
     School school;
+    // relationship between class and student
+    @OneToOne(mappedBy = "schoolClass")
+    Student student;
 }
--- school-system/src/main/java/com/example/school/system/models/Student.java
+++ school-system/src/main/java/com/example/school/system/models/Student.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -47,5 +48,9 @@ public class Student {
     // relationship between student and marks
     @OneToMany
     List<Marks> marks;
+    // relationship between student and class
+    @OneToOne
+    @JoinColumn(name = "class_id")
+    SchoolClass schoolClass;
 
 }
