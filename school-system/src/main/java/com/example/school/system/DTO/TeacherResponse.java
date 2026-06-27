4c6590a06deac6e2e858a02a2f47116689846215
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-27 15:28:11 +0300

    Teacher account creation response altered

---
+++ school-system/src/main/java/com/example/school/system/DTO/TeacherResponse.java
@@ -0,0 +1,10 @@
+package com.example.school.system.DTO;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeacherResponse {
+    public String accountCreationResponse() {
+        return "Account created successfully";
+    }
+}
--- school-system/src/main/java/com/example/school/system/controller/SignUp.java
+++ school-system/src/main/java/com/example/school/system/controller/SignUp.java
@@ -3,6 +3,7 @@ package com.example.school.system.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.school.system.DTO.CreateTeacherDTO;
+import com.example.school.system.DTO.TeacherResponse;
 import com.example.school.system.error.SchoolNotFoundExceptionHandler;
 import com.example.school.system.error.UserExistsExceptionHandler;
 import com.example.school.system.models.School;
@@ -19,16 +20,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class SignUp {
     private final TeacherRepository teacherRepository;
     private final SchoolRepository schoolRepository;
+    private final TeacherResponse teacherResponse;
 
-    public SignUp(TeacherRepository teacherRepository, SchoolRepository schoolRepository) {
+    public SignUp(TeacherRepository teacherRepository, SchoolRepository schoolRepository,
+            TeacherResponse teacherResponse) {
         this.teacherRepository = teacherRepository;
         this.schoolRepository = schoolRepository;
+        this.teacherResponse = teacherResponse;
     }
 
     @PostMapping("/api/create-account")
-    public CreateTeacherDTO createAccount(@Valid @RequestBody CreateTeacherDTO teacherDto) {
+    public String createAccount(@Valid @RequestBody CreateTeacherDTO teacherDto) {
         teacherRepository.save(toTeacher(teacherDto));
-        return teacherDto;
+        return teacherResponse.accountCreationResponse();
     }
 
     private Teacher toTeacher(CreateTeacherDTO teacherCreateTeacherDTO) {
