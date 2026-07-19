a74559eeb48f621c27f2123867dab855d660308d
Carlos Maina <softwarewebdevelopers1@gmail.com>
2026-07-19 21:42:19 +0300

    pagination fixed

--- school-system/src/main/java/com/example/school/system/controller/GetStudentsController.java
+++ school-system/src/main/java/com/example/school/system/controller/GetStudentsController.java
@@ -1,15 +1,12 @@
 package com.example.school.system.controller;
 
 import java.util.List;
-
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.school.system.DTO.GetStudentsOfSpecificClass;
-import com.example.school.system.models.StudentProfile;
 import com.example.school.system.services.SchoolClassService;
 
 import jakarta.validation.Valid;
@@ -24,8 +21,8 @@ public class GetStudentsController {
     @GetMapping("/get/students")
     public List<?> GetStudentsOfSpecificClass(
             @Valid @RequestBody GetStudentsOfSpecificClass getStudentsOfSpecificClass,
-            @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size,
-            @RequestParam(defaultValue = "name,asc") String sort) {
+            @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size
+           ) {
         return schoolClassService.getStudentByClass(getStudentsOfSpecificClass, page, size);
     }
 }
